54a7e78fde3d53685c7b766226cf79a226f16f62
JavCho23 <javier120699lili@gmail.com>
2019-05-15 21:50:17 -0500

    Total de egresos

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var max = 0 ;
 var min;
 var sw = true;
+var total = 0;
+var egresos = [];
 function registrar() {
     let tipo = document.frmEgresos.cdbTipo.value;
-    let monto = document.getElementById("txtMonto").value;
+    let monto = parseFloat(document.getElementById("txtMonto").value);
     let lista = document.getElementById("listadoEgresos");
-
+    total += monto ;
     if (max < monto) {
         max= monto;
     }
@@ -17,6 +19,6 @@ function registrar() {
         min=monto
     }
     let maxmin = document.getElementById("Maxmin");
-    maxmin.innerHTML = '<div style="color:white;"> Maximo Egreso: ' + max  +' Minimo: '+ min +  '</div>';
+    maxmin.innerHTML = '<div style="color:white;"> Total: '+ total+' Maximo Egreso: ' + max  +' Minimo: '+ min +  '</div>';
     lista.innerHTML = lista.innerHTML + ' <div style="color:white;"> Tipo: ' + tipo  +' Monto: '+ monto +  '</div>'; 
 }
